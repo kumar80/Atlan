8faf6f7194319cd09d9cea11f024541839ad97fc
Maverick / <chandankumar80567@gmail.com>
2020-02-16 21:52:29 +0530

    final commit

--- server.js
+++ server.js
@@ -1,69 +1,143 @@
-const http = require('http');
-const static = require('node-static');
-const fileServer = new static.Server('.');
-const path = require('path');
-const fs = require('fs');
-const uploads = Object.create(null);
+let http = require('http');
+let static = require('node-static');
+let fileServer = new static.Server('.');
+let path = require('path');
+let fs = require('fs');
+//let console.log = require('console.log')('example:resume-upload');
 
-function onUpload (req,res) {
-    const fileId = req.headers['file-id'];
-    const start = +req.headers['start'];
+let uploads = Object.create(null);
 
-    if(!fileId)   res.writeHead(400, "No file id");
+let dir = "./";
 
-    let filePath = path.join('./',fileId);
+function onUpload(req, res) {
 
-    if(!uploads[fileId]) uploads[fileId] = {};
-    let upload = uploads[fileId];
-    let fileStream;
-    if(!start){
-        upload.bytesReceived =0;
-        fileStream = fs.createWriteStream(filePath, { flags:'w'});
+  let fileId = req.headers['x-file-id'];
+  let startByte = +req.headers['x-start-byte'];
+
+  if (!fileId) {
+    res.writeHead(400, "No file id");
+    res.end();
+  }
+
+  // we'll files "nowhere"
+  //let filePath = '/dev/null';
+  // could use a real path instead, e.g.
+   let filePath = path.join(dir, fileId);
+
+  console.log("onUpload fileId: "+ fileId);
+
+  // initialize a new upload
+  if (!uploads[fileId]) uploads[fileId] = {};
+  let upload = uploads[fileId];
+
+  console.log("bytesReceived:" + upload.bytesReceived + " startByte:" + startByte)
+
+  let fileStream;
+
+  // if startByte is 0 or not set, create a new file, otherwise check the size and append to existing one
+  if (!startByte) {
+    upload.bytesReceived = 0;
+    fileStream = fs.createWriteStream(filePath, {
+      flags: 'w'
+    });
+    console.log("New file created: " + filePath);
+  } else {
+    // we can check on-disk file size as well to be sure
+    if (upload.bytesReceived != startByte) {
+      res.writeHead(400, "Wrong start byte");
+      res.end(upload.bytesReceived);
+      return;
     }
-    else {
-        if(upload.bytesReceived!=start) {
-            res.writeHead(400,"Retry Upload");
-            res.end(upload.bytesReceived);
-        }
-        fileStream = fs.createWriteStream(filePath, {flage :'a'});
-    }
+    // append to existing file
+    fileStream = fs.createWriteStream(filePath, {
+      flags: 'a'
+    });
+    console.log("File reopened: " + filePath);
+  }
 
-    req.on('data',(data)=>{
-        upload.bytesReceived+=data.length;
-    })
-    req.pipe(fileStream);
-    fileStream.on('close',()=>{
-        if(upload.bytesReceived == req.headers['file-size']){
-            delete uploads[fileId];
-            res.end("success" + upload.bytesReceived);
-        }
-        else {
-            res.end();
-        }
-    })
-    fileStream.on('error',(err)=>{
-        res.writeHead(500,"ERROR");
+
+  req.on('data', function(data) {
+    console.log("bytes received "+ upload.bytesReceived);
+    upload.bytesReceived += data.length;
+  });
+
+  // send request body to file
+  req.pipe(fileStream);
+
+  // when the request is finished, and all its data is written
+  fileStream.on('close', function() {
+    if (upload.bytesReceived == req.headers['x-file-size']) {
+      console.log("Upload finished");
+      delete uploads[fileId];
+      // can do something else with the uploaded file here
+    //  uploads = Object.create(null);
+      res.end("Success " + upload.bytesReceived);
+    } else {
+      // connection lost, we leave the unfinished file around
+      console.log("File unfinished, stopped at " + upload.bytesReceived);
+      res.end();
+    }
+  });
+
+  // in case of I/O error - finish the request
+  fileStream.on('error', function(err) {
+    console.log("fileStream error");
+    res.writeHead(500, "File error");
+    res.end();
+  });
+
+}
+
+function onStatus(req, res) {
+  let fileId = req.headers['x-file-id'];
+  let upload = uploads[fileId];
+  console.log("onStatus fileId: " + fileId +  " upload:" +  upload);
+  if (typeof upload == 'undefined') {
+    res.end("0")
+  } else {
+  //  res.end("0")
+    res.end(String(upload.bytesReceived));
+  }
+}
+
+
+function accept(req, res) {
+
+  if(req.headers['x-quit'] == "1" ) {
+    console.log("quitting")
+    fs.access(req.headers['x-file-id'], fs.F_OK, (err) => {
+      if (err) {
+        console.error(err)
+        return
+      }    
+      //file exists
+      fs.unlink(req.headers['x-file-id'], function (err) {
+        if (err) throw err;
+        // if no error, file has been deleted successfully
+        console.log('File deleted!');
         res.end();
+    }); 
     })
+  }
+
+  if (req.url == '/status') {
+    onStatus(req, res);
+  } else if (req.url == '/upload' && req.method == 'POST') {
+    onUpload(req, res);
+  } else {
+    fileServer.serve(req, res);
+  }
+
 }
 
-function onStatus(req,res) {
-    const fileId = req.headers['file-id'];
-    const upload = uploads[fileId];
-    if(!upload) res.end("0")
-    else res.end(bytesReceived.toString(10));
-}
 
-function accept(req,res) {
-    if(req.url == '/status') onStatus(req,res);
-    else if(req.url == '/upload' && req.method == 'POST') onUpload(req,res);
-    else fileServer.serve(req,res);
-}
 
-if(!module.parent){
-    http.createServer(accept).listen(8080);
-        console.log('Server at 8080');
-}
-else {
-    exports.accept = accept;
+
+// -----------------------------------
+
+if (!module.parent) {
+  http.createServer(accept).listen(8080);
+  console.log('Server listening at port 8080');
+} else {
+  exports.accept = accept;
 }
--- uploader.js
+++ uploader.js
@@ -1,40 +1,88 @@
-class uploader {
-    constructor ({file,progress}) {
-        this.file = file;
-        this.progress = progress;
-        this.fileId = file.name + '_' + file.lastModifiedDate;
+class Uploader {
+  
+  constructor({file, onProgress}) {
+    this.file = file;
+    this.onProgress = onProgress;
+    // create fileId that uniquely identifies the file
+    // we could also add user session identifier (if had one), to make it even more unique
+    this.fileId = file.name + '-' + file.size + '-' + +file.lastModifiedDate;
+  }
+
+  async getUploadedBytes() {
+    let response = await fetch('status', {
+      headers: {
+        'X-File-Id': this.fileId
+      }
+    });
+
+    if (response.status != 200) {
+      throw new Error("Can't get uploaded bytes: " + response.statusText);
     }
 
-    async uploadStatus(){
-        let res  = await fetch('status',{headers :{ 'file-id' : this.fileId} });
-        if(res.status!=200){
-            throw new Error(res.statusText);
+    let text = await response.text();
+
+    return +text;
+  }
+
+  async upload() {
+    this.startByte = await this.getUploadedBytes();
+
+    let xhr = this.xhr = new XMLHttpRequest();
+    xhr.open("POST", "upload", true);
+
+    // send file id, so that the server knows which file to resume
+    xhr.setRequestHeader('X-File-Id', this.fileId);
+    // send the byte we're resuming from, so the server knows we're resuming
+    xhr.setRequestHeader('X-Start-Byte', this.startByte);
+    xhr.setRequestHeader('X-quit', "0");
+    xhr.setRequestHeader('x-file-size', this.file.size);
+
+    xhr.upload.onprogress = (e) => {
+      this.onProgress(this.startByte + e.loaded, this.startByte + e.total);
+    };
+
+    console.log("send the file, starting from", this.startByte);
+    xhr.send(this.file.slice(this.startByte));
+
+    // return
+    //   true if upload was successful,
+    //   false if aborted
+    // throw in case of an error
+    return  succes = await new Promise((resolve, reject) => {
+
+      xhr.onload = xhr.onerror = () => {
+        console.log("upload end status:" + xhr.status + " text:" + xhr.statusText);
+
+        if (xhr.status == 200) {
+          resolve(true);
+        } else {
+          reject(new Error("Upload failed: " + xhr.statusText));
         }
-        let txt = await res.text();
-        return  +txt;
+      };
+
+      // onabort triggers only when xhr.abort() is called
+      xhr.onabort = () => resolve(false);
+
+    });
+
+  }
+
+  stop() {
+    if (this.xhr) {      
+      this.xhr.abort();
     }
+  }
 
-    async upload(){
-        this.start = await this.uploadStatus();
-        let req = this.req = new XMLHttpRequest();
-        req.open("POST","upload",true);
-        req.setRequestHeader('file-id',this.fileId);
-        req.setRequestHeader('start',this.start);
-        req.upload.onprogress = (event) =>{
-                this.progress(this.start + event.loaded,this.start + event.total);
-        }
-        req.send(this.file.slice(this.start));
-
-        return await new Promise((res,rej)=>{
-            req.onload = req.onerror = ()=> {
-                if(req.status == 200) res(true);
-                else 
-                    rej(new Error(req.statusText));
-            }
-            req.onabort = ()=>res(false);
+   quit() {
+     if(success)
+        let response =  fetch('status', {
+          headers: {
+            'X-quit': "1",
+            'x-file-id' : this.fileId
+          }
         })
-    }
-    stop() {
-        if(this.req) this.req.abort();
-    }
+        .then(()=>this.stop())
+        .catch(err=> console.log(err))
+      }
+    
 }
